271883c0431e1104a5fe72166c27ee555703567f
mikechess <mikecook450@gmail.com>
2024-02-06 15:23:23 -0500

    03.Declaring Types For API Data And Creating UI & Rendering API Data

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -3,15 +3,22 @@ import { MoviesWrapper } from '../movies.modules'
 import axios from 'axios'
 import { apiKey, discoverEndpoint } from '../api_links'
 
+interface MovieTypes {
+    id: number;
+    title: string;
+    poster_path: string;
+    release_date: string;
+}
+
 const Movies = () => {
 
-    const [showItems, setShowItems] = useState();
+    const [showItems, setShowItems] = useState<MovieTypes[]>([]);
 
     useEffect(() => {
         const fetchMovies = async () => {
             try {
                 const response = await axios.get(`${discoverEndpoint}?api_key=${apiKey}`);
-                const results = response.data;
+                const {results} = response.data;
                 setShowItems(results);
                 console.log(results);
             } catch (error) {
@@ -23,7 +30,26 @@ const Movies = () => {
     }, [])
 
   return (
-    <MoviesWrapper></MoviesWrapper>
+    <MoviesWrapper>
+        <h1>TSX-flix Prime</h1>
+        <div className="movieCard">
+            {showItems.map((items) => {
+                return (
+                    <div className="movie" key={items.id}>
+                        <div className="movieImg">
+                            <img src={`https://image.tmdb.org/t/p/w200/${items.poster_path}`} alt="Poster" />
+                        </div>
+
+                        <div className="movieInfo">
+                            <h4>{items.title}</h4>
+
+                            <p>{items.release_date}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    </MoviesWrapper>
   )
 }
 
