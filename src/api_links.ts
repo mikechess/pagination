4f9950ed43ab05a36f486e0f77c3a7666703e460
mikechess <mikecook450@gmail.com>
2024-02-06 14:51:57 -0500

    01.Project Setup And Practical Example In Vs Code

---
+++ src/api_links.ts
@@ -0,0 +1,4 @@
+export const apiKey = "e46bbbed5a14602bf1d6cba23b5305dd";
+
+
+export const discoverEndpoint = "https://api.themoviedb.org/3/discover/movie";
---
+++ src/components/Movies.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { MoviesWrapper } from '../movies.modules'
+import axios from 'axios'
+
+const Movies = () => {
+
+    const [showItems, setShowItems] = useState()
+
+  return (
+    <MoviesWrapper></MoviesWrapper>
+  )
+}
+
+export default Movies
